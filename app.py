607b09ea943eb3a0261643d264f553106464fe48
pdhey <162877404+pdhey@users.noreply.github.com>
2024-05-18 04:36:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ Original file is located at
 import streamlit as st
 import tensorflow as tf
 
-@st.cache(allow_output_mutation=True)
+@st.cache_data(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('cats_and_dogs_small_2.h5')
+  model=tf.keras.models.load_model('cats_and_dogs_small_4.h5')
   return model
 model=load_model()
 st.write("""
